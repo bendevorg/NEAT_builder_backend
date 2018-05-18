c177ea4297e8f1c6126098189bdd4437dc498fd6
Guilherme Conti <guibasconti@gmail.com>
2018-05-17 21:18:43 -0300

    Remove temp leaderboard var

--- server/core/routers/leaderboard.js
+++ server/core/routers/leaderboard.js
@@ -5,14 +5,6 @@ const fs = require('fs');
 const controllersPath = process.cwd() + '/server/controllers/leaderboard';
 let controllers = [];
 
-//  THIS WILL BE REMOVED WHEN I IMPLEMENT DATABASES
-global.tempLeaderboard = [];
-tempLeaderboard["1"] = [{
-  name: 'Casual',
-  score: 1,
-  time: 1
-}];
-
 // Get our routers
 fs.readdirSync(controllersPath).forEach(file => {
   if (file.indexOf('.js') !== -1)
