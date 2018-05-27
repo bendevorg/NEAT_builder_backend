d150cc693f9b8f5cbb7b42c6ea7d1c79a72fe934
Guilherme Conti <guibasconti@gmail.com>
2018-05-27 20:52:19 -0300

    Rename API to /instructions

---
+++ server/controllers/game/retrieveInstructions.js
@@ -0,0 +1,52 @@
+/**
+ * Module to retrive game instructions
+ * @module controllers/game/retrieveInstructions
+ */
+const logger = require('../../../tools/logger');
+const database = require('../../models/database');
+const validator = require('../../utils/validator');
+const constants = require('../../utils/constants');
+
+/**
+ * Retrieve game instructions
+ * Get a game based on a gameid
+ *
+ * @param {string} req.params.gameId - Game ID
+ * @return {object} - Returns the leaderboard in a json format
+ * @throws {object} - Returns a msg that indicates a failure
+ * 
+ */
+module.exports = (req, res) => {
+  let {gameId} = req.params;
+  if (!validator.isValidString(gameId))
+    return res.status(400).json({
+      msg: constants.messages.error.INVALID_GAME_ID
+    });
+    gameId = gameId.trim();
+
+  database.instructions.findAll( 
+    {
+      attributes:['name'],
+      include: [{
+        model: database.instructionItems, 
+        attributes:['name','description']
+      }],
+      where: {
+        gameId: gameId
+      }
+    })
+    .then(instructions => {
+      if (!instructions)
+        return res.status(400).json({
+          msg: constants.messages.error.INVALID_GAME_ID
+        });
+      return res.status(200).json({msg: instructions});
+    }) 
+    .catch(database.sequelize.Sequelize.DatabaseError, err => {
+      return res.status(400).json({msg: constants.messages.error.INVALID_GAME_ID});
+    })
+    .catch(err => {
+      return res.status(500).json({msg: constants.messages.error.UNEXPECTED_DB});
+    });
+
+};
