e6afc812ec9352934354e6b7f03e8b08b99620d7
Guilherme Conti <guibasconti@gmail.com>
2018-06-14 21:01:16 -0300

    Fix sequelize setup for windows

--- server/models/database.js
+++ server/models/database.js
@@ -7,12 +7,12 @@ const path = require('path');
 const basename = path.basename(module.filename);
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.database,
-  process.env.username,
-  process.env.password,
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.host,
-    dialect: process.env.dialect,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
     pool: {
       max: 5,
       min: 0,
